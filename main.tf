e8178df72765371a4bf00a37cac3a6e1618543ea
Patil <mangesh.a.patil@sogeti.com>
2020-07-13 21:17:33 +0200

    with map

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
+
 provider "aws" {
   version = "~> 2.70"
-  region  = var.region
+  region  = local.cfg["region"]
 }
 
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "${terraform.workspace}.${var.vpc_name}"
-  cidr = "10.0.0.0/16"
+  name = local.cfg["network"]["vpc_name"]
+  cidr = local.cfg["network"]["cidr"]
 
   azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  #public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = local.cfg["network"]["private_subnets"]
+  
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
